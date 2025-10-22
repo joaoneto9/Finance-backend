c9685481b7b7ff5efd8548fa8c839900ba6af357
joaoneto9 <joaonetocg1@gmail.com>
2025-10-21 23:31:07 -0300

    feat: seting endpoints of the User

--- fachade.py
+++ fachade.py
@@ -1,5 +1,14 @@
-from flask import Flask
+from tkinter.tix import Tree
+from flask import Flask, jsonify
+
+user_enpoint = "/user"
 
 app = Flask(__name__)
 
+@app.get(user_enpoint)
+def get_user():
+    return jsonify(), 200
 
+@app.post(user_enpoint)
+def save_user():
+    return jsonify(True), 200
--- model/User.py
+++ model/User.py
@@ -1,4 +1,6 @@
 class User:
 
-    def __init__(self, name, email, ) -> None:
-        pass
+    def __init__(self, username: str, email: str, password: str) -> None:
+        self.username = username
+        self.email = email
+        self.password = password
